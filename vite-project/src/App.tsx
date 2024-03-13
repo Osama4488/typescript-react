96ce6546d70d2248d468a6088bbcb505fee6b274
Osama4488 <syed.osama.ali.96@gmail.com>
2024-03-13 16:13:50 +0500

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Add from "./components/Add";
+import List from "./components/List";
+function App() {
+  return (
+    <div className="">
+      <Add />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/Add.tsx
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import { useAppDispatch } from "../store/hooks";
+import { addPerson } from "../store/slices/personSlice";
+
+function Add() {
+  const dispatch = useAppDispatch();
+  const name = useRef<string>("");
+
+  return (
+    <div className="border rounded-md p-2 shadow-md m-2">
+      <label htmlFor="">Person Name:</label>
+      <input
+        className="border rounded-md p-2 mx-2"
+        onChange={(e) => (name.current = e.target.value)}
+      />
+      <button
+        onClick={() => dispatch(addPerson({ name: name.current }))}
+        className="bg-violet-500  text-white rounded-md px-4 py-2 cursor-pointer hover:bg-violet-600 active:bg-violet-700"
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default Add;
